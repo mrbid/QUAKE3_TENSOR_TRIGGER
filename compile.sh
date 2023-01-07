01f592e84b13bc96d280561f73f0b2752e97c9eb
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2023-01-07 12:32:31 +0000

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-clang qaim.c -Ofast -march=native -mfma -lX11 -lxdo -lespeak -lm -o PredictBot/aim
+clang qaim.c -Ofast -mfma -lX11 -lxdo -lespeak -lm -o PRED_FNN/aim
+clang qaim.c -Ofast -mfma -lX11 -lxdo -lespeak -lm -o PRED_CNN/aim
